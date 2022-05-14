fc17ab8692cbb6bcf0e9111cfb704fddd457d556
Francisco Cortez <franciscocortezdev@gmail.com>
2022-05-14 17:46:52 -0600

    functionality game

--- code.js
+++ code.js
@@ -1,8 +1,10 @@
 const cells = document.querySelectorAll('.cell');
 const btnStart = document.querySelector('.btn_start').addEventListener('click',startGame);
 const winOptions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-const cellsActive = [];
-let winCells = [];
+const cellsActiveX = [];
+const cellsActiveO = [];
+let winCellsX = [];
+let winCellsO = [];
 let turnPlayerX = true;
 let GameLive = false;
 
@@ -15,11 +17,19 @@ function startGame() {
         cells.forEach((cell)=>{
             cell.addEventListener('click',(eCell)=>{
                 if(eCell.target.innerHTML == '' && GameLive == true){
-                    eCell.target.innerHTML = turnPlayerX ? 'X': 'O';
-        
-                    cellsActive.push(Number(eCell.target.dataset.cell))
-                    turnPlayerX = !turnPlayerX;
-                    evalGame();
+                    if (turnPlayerX){
+                        cellsActiveX.push(Number(eCell.target.dataset.cell))
+                        eCell.target.innerHTML = 'X';
+                        turnPlayerX = !turnPlayerX;
+                        evalGame();
+                    }else{
+                        eCell.target.innerHTML = 'O';
+                        cellsActiveO.push(Number(eCell.target.dataset.cell))
+                        turnPlayerX = !turnPlayerX;
+                        evalGame();
+
+                    }
+                    
                     
         
         
@@ -38,26 +48,37 @@ const evalGame = () =>{
     
     for (let i = 0; i < winOptions.length; i++) {        
             for (let j = 0; j < winOptions[i].length; j++) {
-                if(cellsActive.includes(winOptions[i][j])){
-                    winCells.push(winOptions[i][j])
+                if(cellsActiveX.includes(winOptions[i][j])){
+                    winCellsX.push(winOptions[i][j])
+                }
+                if(cellsActiveO.includes(winOptions[i][j])){
+                    winCellsO.push(winOptions[i][j])
                 }
             }
-        if(winCells.length == 3){
+        if(winCellsX.length == 3){
+            console.log('Ganaste');
+            endGame();
+            break;
+        } 
+        if(winCellsO.length == 3){
             console.log('Ganaste');
             endGame();
             break;
         } 
         
-        winCells = [];
+        winCellsX = [];
+        winCellsO = [];
     }
-    return winCells;
+    
 }
 
 
 const endGame = ()=>{
     GameLive = false;
-    cellsActive.length = 0;
-    winCells = [];
+    cellsActiveX.length = 0;
+    cellsActiveO.length = 0;
+    winCellsX = [];
+    winCellsO = [];
     turnPlayerX = true;
 
 }
