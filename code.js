6d3fe264e49a47770ffa4a37d132b035fd09946e
Francisco Cortez <franciscocortezdev@gmail.com>
2022-05-15 14:32:54 -0600

    tied game functionality

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const cells = document.querySelectorAll('.cell');
-const player = document.querySelector('.head_playerTurn');
+const playerWinner = document.querySelector('.head_player');
 const btnStart = document.querySelector('.btn_start').addEventListener('click',startGame);
 const winOptions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 const cellsActiveX = [];
@@ -9,12 +9,17 @@ let winCellsO = [];
 let turnPlayer = true;
 let GameLive = false;
 
-    
+
+
 function startGame() {
     cells.forEach(cel=> cel.innerHTML = '');
-     
+    playerWinner.classList.remove('winner');
+    playerWinner.classList.remove('empate');
+    playerWinner.innerHTML = 'Turno del jugador: X';
+    winCellsX = [];
+    winCellsO = [];
     GameLive = true;
-   
+    
         cells.forEach((cell)=>{
             cell.addEventListener('click',(eCell)=>{
                 if(eCell.target.innerHTML == '' && GameLive == true){
@@ -23,15 +28,19 @@ function startGame() {
                         eCell.target.innerHTML = 'X';
                         playerTurn();
                         evalGame();
+                        
                     }else{  
                         eCell.target.innerHTML = 'O';
                         cellsActiveO.push(Number(eCell.target.dataset.cell))
                         playerTurn();
                         evalGame();
-
+                       
                     }
-                }   
+
+                }
+
             }) 
+            
         })
 }
 
@@ -50,16 +59,22 @@ const evalGame = () =>{
                 }
             }
         if(winCellsX.length == 3){
-            console.log('Ganaste');
+            winGame();
             endGame();
             break;
         } 
         if(winCellsO.length == 3){
-            console.log('Ganaste');
+            winGame();
             endGame();
             break;
         } 
-        
+        let totalCells = [...cellsActiveX, ...cellsActiveO];
+        if (totalCells.length == 9){
+            winGame();
+            endGame();
+        }
+
+
         winCellsX = [];
         winCellsO = [];
     }
@@ -71,8 +86,6 @@ const endGame = ()=>{
     GameLive = false;
     cellsActiveX.length = 0;
     cellsActiveO.length = 0;
-    winCellsX = [];
-    winCellsO = [];
     turnPlayer = true;
 
 }
@@ -80,14 +93,27 @@ const endGame = ()=>{
 function playerTurn(){
     
     if(turnPlayer){
-        player.innerHTML = 'O'
+        playerWinner.innerHTML = 'Turno del jugador: O';
         turnPlayer = !turnPlayer;
     }else{
-        player.innerHTML = 'X'
+        playerWinner.innerHTML = 'Turno del jugador: X';
         turnPlayer = !turnPlayer;
     }
-        
+}
 
+const winGame = ()=>{
+    if (winCellsX.length == 3){
+        playerWinner.classList.add('winner');
+        playerWinner.innerHTML = 'Ganador jugador: X';
+
+    }else if(winCellsO.length == 3){
+        playerWinner.classList.add('winner');
+        playerWinner.innerHTML = 'Ganador jugador: O';
+    }else if(winCellsX.length<3 && winCellsO.length<3){
+        playerWinner.classList.add('empate');
+        playerWinner.innerHTML = 'Juego empatado';
+
+    }
 }
 
 
