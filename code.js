58d386e43adf629cfbfb4b01dadb08a48bad7f7f
Francisco Cortez <franciscocortezdev@gmail.com>
2022-05-16 13:20:30 -0600

    Bug TurnPlayer fixed

--- code.js
+++ code.js
@@ -10,12 +10,11 @@ let turnPlayer = true;
 let GameLive = false;
 
 
-
 function startGame() {
   cells.forEach(cel=> cel.innerHTML = '');
   playerWinner.classList.remove('winner');
   playerWinner.classList.remove('empate');
-  playerWinner.innerHTML = 'Turno del jugador: X';
+  playerTurn();
   cellsActiveX.length = 0;
   cellsActiveO.length = 0;
   winCellsX = [];
@@ -23,7 +22,6 @@ function startGame() {
   GameLive = true;
     
   cells.forEach((cell)=>{
-    cell.classList.remove('win');
     cell.addEventListener('click',(eCell)=>{
       if(eCell.target.innerHTML == '' && GameLive == true){
         if (turnPlayer){
@@ -51,43 +49,41 @@ function startGame() {
 
 
 const evalGame = () =>{
-    
-  for (let i = 0; i < winOptions.length; i++) {        
-      for (let j = 0; j < winOptions[i].length; j++) {
-        if(cellsActiveX.includes(winOptions[i][j])){
-          winCellsX.push(winOptions[i][j])
+    for (let i = 0; i < winOptions.length; i++) {        
+        for (let j = 0; j < winOptions[i].length; j++) {
+          if(cellsActiveX.includes(winOptions[i][j])){
+            winCellsX.push(winOptions[i][j])
+          }
+          if(cellsActiveO.includes(winOptions[i][j])){
+            winCellsO.push(winOptions[i][j])
+          }
         }
-        if(cellsActiveO.includes(winOptions[i][j])){
-          winCellsO.push(winOptions[i][j])
-        }
-      }
-    if(winCellsX.length == 3){
-      winGame();
-      endGame();
-      break;
-    } 
-    if(winCellsO.length == 3){
-      winGame();
-      endGame();
-      break;
-    } 
-    let totalCells = [...cellsActiveX, ...cellsActiveO];
+      if(winCellsX.length == 3){
+        winGame();
+        endGame();
+        break;
+      } 
+      if(winCellsO.length == 3){
+        winGame();
+        endGame();
+        break;
+      } 
+      winCellsX = [];
+      winCellsO = [];
+    }
+  let totalCells = [...cellsActiveX, ...cellsActiveO];
     if (totalCells.length == 9){
       winGame();
       endGame();
+      return;
     }
-
-
-    winCellsX = [];
-    winCellsO = [];
-  }
     
 }
 
 
 const endGame = ()=>{
   GameLive = false;
-  turnPlayer = true;
+  turnPlayer = !turnPlayer;
 
 }
 
@@ -106,32 +102,31 @@ const winGame = ()=>{
   if (winCellsX.length == 3){
     playerWinner.classList.add('winner');
     playerWinner.innerHTML = 'Ganador jugador: X';
-    let cellWin1 = document.querySelector(`[data-P="${winCellsX[0]}"]`)
-    let cellWin2 = document.querySelector(`[data-P="${winCellsX[1]}"]`)
-    let cellWin3 = document.querySelector(`[data-P="${winCellsX[2]}"]`)
 
-    cellWin1.classList.add('win');
-    cellWin2.classList.add('win');
-    cellWin3.classList.add('win');
+    winCellsX.forEach((num)=>{
+      document.querySelector(`[data-P="${num}"]`).classList.add('win');
+    })
+
+
 
   }else if(winCellsO.length == 3){
     playerWinner.classList.add('winner');
     playerWinner.innerHTML = 'Ganador jugador: O';
 
-    let cellWin1 = document.querySelector(`[data-P="${winCellsO[0]}"]`)
-    let cellWin2 = document.querySelector(`[data-P="${winCellsO[1]}"]`)
-    let cellWin3 = document.querySelector(`[data-P="${winCellsO[2]}"]`)
+    winCellsO.forEach((num)=>{
+      document.querySelector(`[data-P="${num}"]`).classList.add('win');
+    })
 
-    cellWin1.classList.add('win');
-    cellWin2.classList.add('win');
-    cellWin3.classList.add('win');
 
 
   }else if(winCellsX.length<3 && winCellsO.length<3){
     playerWinner.classList.add('empate');
     playerWinner.innerHTML = 'Juego empatado';
-
-
+    let totalCellsP = [...cellsActiveX, ...cellsActiveO];
+    
+    totalCellsP.forEach((num)=>{
+      document.querySelector(`[data-P="${num}"]`).classList.add('win');
+    })
 
   }
 }
